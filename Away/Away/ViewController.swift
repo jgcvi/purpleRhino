176e048847ecc0638070b9bdf00852e54d5975eb
Samantha Konami Richards <samanthakrichards@gmail.com>
2016-08-29 13:33:14 -0700

    Added Tinder card animation files

--- Away/Away/ViewController.swift
+++ Away/Away/ViewController.swift
@@ -2,24 +2,27 @@
 //  ViewController.swift
 //  Away
 //
-//  Created by Shabeeb Shah on 8/29/16.
-//  Copyright © 2016 American Express. All rights reserved.
+//  Created by Gao Chao on 4/30/15.
+//  Copyright (c) 2015 gcweb. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let draggableBackground: DraggableViewBackground = DraggableViewBackground(frame: self.view.frame)
+        self.view.addSubview(draggableBackground)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
